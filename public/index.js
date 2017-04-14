8ea1a7831806bd9bf24d6ba9c4d7cda472f43437
Hugo Yu <guo.yu@uwaterloo.ca>
2017-04-13 23:31:49 -0400

    Set up

---
+++ public/index.js
@@ -0,0 +1,161 @@
+var HOST_NAME = "http://localhost:4000/";
+
+var DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '191281804711015',
+        xfbml: true,
+        version: 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+    fbAuthInit();
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+fbAuthInit = function() {
+    FB.login(function(response) {
+        if (response.authResponse) {
+            onloadPage();
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    });
+}
+
+function onloadPage() {
+    loadFeed(null, processData);
+}
+
+function loadFeed(url, _callback) {
+    if (!url) {
+        url = "/372772186164295/feed";
+    }
+    FB.api(
+        url,
+        function(response) {
+            _callback(response);
+        });
+}
+
+function processData(response) {
+    var post = response.data[Math.floor(Math.random() * 24)];
+
+    var message = post.message.replace(/(\r\n|\n|\r)/gm, " "); // remove \n
+    message = message.replace(/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g, '') // remove emoji
+
+    var updatedTime = post.updated_time;
+
+    var posts = [{
+        "message": "Looking for a ride from Scarbrough to Waterloo Friday afternoon.",
+        "updatedTime": "2017-04-13T18:35:33+0000"
+    }];
+    posts = [];
+
+
+    posts.push({
+        "message": message,
+        "updatedTime": updatedTime
+    });
+
+    posts.forEach(function(post) {
+        console.log(post);
+
+        $.ajax(HOST_NAME + 'process', {
+            dataType: 'json',
+            headers: {
+                'text': post.message,
+                'time': post.updatedTime
+            },
+            success: function(response) {
+                var data = response.data;
+                console.log(data);
+
+                var date = new Date(data.date);
+                // user-friendly time
+                var timeString;
+                var hour = date.getHours();
+                var am = true;
+                if (hour >= 12) {
+                    am = false;
+                    if (hour > 12) {
+                        hour -= 12;
+                    }
+                }
+                if (hour == 0) {
+                    hour = 12;
+                }
+                timeString = hour + ":" + ("0" + date.getMinutes()).slice(-2) + (am ? " AM" : " PM");
+
+                if (timeString == "12:00 AM") {
+                    timeString = "N/A";
+                }
+
+                var $scope = $('body').scope();
+                if (!$scope.carpoolRecords) {
+                    $scope.carpoolRecords = [];
+                }
+
+                data.routes.forEach(function(route) {
+                    var post = {
+                        "originLine1": route.origin_locs[0],
+                        "originLine2": route.origin_locs[1] ? route.origin_locs[1] : "",
+                        "originCode": route.origin_area,
+                        "destinationLine1": route.dest_locs[0],
+                        "destinationLine2": route.dest_locs[1] ? route.dest_locs[1] : "",
+                        "destinationCode": route.dest_area,
+                        "dateRaw": data.date,
+                        "date": MONTHS[date.getMonth()] + " " + date.getDate(),
+                        "time": timeString,
+                        "day": DAYS[date.getDay()],
+                        "price": data.price
+                    };
+                    $scope.carpoolRecords.push(post);
+                });
+
+                $scope.carpoolRecords.sort(function(p1, p2) {
+                    if (p1.originCode == p2.originCode) {
+                        if (p1.destinationCode == p2.destinationCode) {
+                            if (p1.dateRaw == p2.dateRaw) {
+                                return false;
+                            } else {
+                                return p1.dateRaw > p2.dateRaw;
+                            }
+                        } else {
+                            return p1.destinationCode > p2.destinationCode;
+                        }
+                    } else {
+                        return p1.originCode > p2.originCode;
+                    }
+                });
+
+                $scope.$apply();
+            },
+            error: function(response) {
+                console.log(response);
+            }
+        });
+    });
+
+
+
+    // var terms = nlp(message, lexicon).terms().data();
+
+    var nextURL = response.paging.next;
+}
+
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope) {
+
+});
---
+++ public/json/app.js
@@ -0,0 +1,491 @@
+var stringMatchTol = 0.9;
+
+var express = require('express');
+var app = express();
+
+var natural = require('natural');
+var nlp = require('compromise');
+var sugar = require('sugar');
+
+var wpTokenizer = new natural.WordPunctTokenizer();
+var tokenizer = new natural.WordTokenizer();
+
+var postTypeClassifier = new natural.BayesClassifier();
+postTypeClassifier.addDocument('driving', 'offer');
+postTypeClassifier.addDocument('looking for', 'request');
+postTypeClassifier.train();
+
+var places = {
+    "bk": { "placeCode": "bk", "areaCode": "wat" },
+    "downtown toronto": { "placeCode": "dt", "areaCode": "trt" },
+    "downtown trt": { "placeCode": "dt", "areaCode": "trt" },
+    "dt toronto": { "placeCode": "dt", "areaCode": "trt" },
+    "dt trt": { "placeCode": "dt", "areaCode": "trt" },
+    "toronto downtown": { "placeCode": "dt", "areaCode": "trt" },
+    "trt downtown": { "placeCode": "dt", "areaCode": "trt" },
+    "toronto dt": { "placeCode": "dt", "areaCode": "trt" },
+    "trt dt": { "placeCode": "dt", "areaCode": "trt" },
+    "finch": { "placeCode": "finch", "areaCode": "nyk" },
+    "fairview": { "placeCode": "fvm", "areaCode": "nyk" },
+    "gta": { "placeCode": "gta", "areaCode": "gta" },
+    // "loo": { "placeCode": "wat", "areaCode": "wat" },
+    "markham": { "placeCode": "mar", "areaCode": "mar" },
+    "mississauga": { "placeCode": "mis", "areaCode": "mis" },
+    "north york": { "placeCode": "nyk", "areaCode": "nyk" },
+    "pacific mall": { "placeCode": "pml", "areaCode": "mar" },
+    "pmall": { "placeCode": "pml", "areaCode": "mar" },
+    "p-mall": { "placeCode": "pml", "areaCode": "mar" },
+    "pearson": { "placeCode": "yyz", "areaCode": "yyz" },
+    "richmond hill": { "placeCode": "ric", "areaCode": "ric" },
+    "rhill": { "placeCode": "ric", "areaCode": "ric" },
+    "scarborough": { "placeCode": "sca", "areaCode": "sca" },
+    "scarborough town centre": { "placeCode": "stc", "areaCode": "sca" },
+    "stc": { "placeCode": "stc", "areaCode": "sca" },
+    "sheppard yonge": { "placeCode": "she", "areaCode": "nyk" },
+    "sq1": { "placeCode": "sq1", "areaCode": "mis" },
+    "square one": { "placeCode": "sq1", "areaCode": "mis" },
+    "toronto": { "placeCode": "trt", "areaCode": "trt" },
+    "trt": { "placeCode": "trt", "areaCode": "trt" },
+    "union": { "placeCode": "uni", "areaCode": "trt" },
+    "university of waterloo": { "placeCode": "uw", "areaCode": "wat" },
+    "uw": { "placeCode": "uw", "areaCode": "wat" },
+    "uwaterloo": { "placeCode": "uw", "areaCode": "wat" },
+    "vaughan": { "placeCode": "vau", "areaCode": "vau" },
+    "waterloo": { "placeCode": "wat", "areaCode": "wat" },
+    "yorkdale": { "placeCode": "ykd", "areaCode": "nyk" }
+};
+
+var placeLvlDict = {
+    "bk": 4,
+    "dt": 3,
+    "finch": 4,
+    "fvm": 4,
+    "gta": 1,
+    "mar": 3,
+    "mis": 3,
+    "nyk": 3,
+    "pml": 4,
+    "ric": 3,
+    "sca": 3,
+    "she": 4,
+    "sq1": 4,
+    "stc": 4,
+    "trt": 2,
+    "uni": 4,
+    "uw": 4,
+    "vau": 3,
+    "wat": 3,
+    "ykd": 4,
+    "yyz": 4
+};
+
+var placeNameDict = {
+    "bk": "Burger King Plaza",
+    "dt": "Downtown Toronto",
+    "finch": "Finch Subway Station",
+    "fvm": "Fairview Mall",
+    "gta": "Greater Toronto Area",
+    "mar": "Markham",
+    "mis": "Mississauga",
+    "nyk": "North York",
+    "pml": "Pacific Mall",
+    "ric": "Richmond Hill",
+    "sca": "Scarborough",
+    "she": "Sheppard Subway Station",
+    "sq1": "Square One",
+    "stc": "Scarborough Town Centre",
+    "trt": "Toronto",
+    "uni": "Union Station",
+    "uw": "University of Waterloo",
+    "vau": "Vaughan",
+    "wat": "Waterloo",
+    "ykd": "Yorkdale",
+    "yyz": "Pearson Airport"
+};
+
+var routePlaceLexicon = {};
+for (var key in places) {
+    routePlaceLexicon[key] = "MyPlace";
+}
+
+var fbGeoInfoLexicon = {
+    'Mississauga': "Place",
+    'Ontario': "Place",
+    'Waterloo': "Place"
+};
+
+var DAYS_OF_WEEK = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+var myDayOfWeekDict = {
+    'mon': 'monday',
+    'tue': 'tuesday',
+    'wed': 'wednesday',
+    'thu': 'thursday',
+    'fri': 'friday',
+    'sat': 'saturday',
+    'sun': 'sunday',
+    'monday': 'monday',
+    'tuesday': 'tuesday',
+    'wednesday': 'wednesday',
+    'thursday': 'thursday',
+    'friday': 'friday',
+    'saturday': 'saturday',
+    'sunday': 'sunday'
+};
+var myDayOfWeekLexicon = {};
+for (var key in myDayOfWeekDict) {
+    myDayOfWeekLexicon[key] = "MyDayOfWeek";
+}
+
+var dateRegExp = /(jan|feb|mar|apr|may|jun|jul|aug|sept|oct|nov|dec|january|febuary|march|april|june|july|august|september|october|november|december) ?[0-9]{1,2}/i;
+var dayOfWeekRegExp = / (mon|tue|wed|thu|fri|sat|sun|monday|tuesday|wednesday|thursday|friday|saturday|sunday) /ig;
+var punctuRegExp = /(\/|\(|\))/g;
+
+// var text = "Looking for Waterloo to Square One on wed tonight at 8:30 $10 - Waterloo, Ontario";
+// console.log(getTimeStr(text));
+
+app.get('/process', function(req, res) {
+    var text = req.headers.text;
+    var updatedTime = req.headers.time;
+    console.log(text);
+
+
+    res.send(parsePost(text, updatedTime));
+})
+
+app.listen(4000, function() {
+    console.log('Example app listening on port 4000!')
+})
+
+// parsePost("Carpool pls FREE - Toronto, Ontario  LOOKING for a ride from UW ---> Toronto (Finch or Sheppard Stn) tonight, anywhere in between 6pm to 7pm.", "2017-04-13T17:44:59+0000");
+function parsePost(text, updatedTime) {
+    // tokenize
+    var textTokens = wpTokenizer.tokenize(text);
+    // console.log(textTokens);
+
+    // // split into origin and dest
+    // var splitRegex = '(>|to)'
+    // var splitResult = nlp(text).splitOn(splitRegex).data();
+
+    // // no "to" found
+    // if (splitResult.length < 3) {
+    //     console.error("No to's");
+    //     return;
+    // }
+
+    // var textAfterTo = splitResult.slice(2).map(function(match) {
+    //     return match.text;
+    // }).join();
+    // // currently only support 1 route (that is, splitResult = [stringBeforeTo, theOnlyTo, stringAfterTo])
+    // if (nlp(textAfterTo).splitOn(splitRegex).data().length >= 3) {
+    //     console.error("Too many to's");
+    //     console.log(nlp(textAfterTo).splitOn(splitRegex).data());
+    //     return;
+    // }
+
+    // var originText = splitResult[0].text.replace(punctuRegExp, " $& "); // surround punctuations by whitespaces 
+    // var destText = splitResult[2].text.replace(punctuRegExp, " $& ");
+    // console.log("originText: " + originText);
+    // // console.log("destText: " + destText);
+
+    // var origins = nlp(originText, routePlaceLexicon).match('#MyPlace').data().map(function(matchToken) {
+    //     return matchToken.normal;
+    // });
+    // // when people omit origins, they mean from uw
+    // if (origins.length == 0) {
+    //     origins = ['uw'];
+    // }
+
+    var textWoBrackets = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    var matchedRoutes = matchWithLexicon(textWoBrackets, "#place#( ?- ?| ?> ?| ?-*> ?| to )#place#", 'i', 0);
+    if (matchedRoutes.length == 0) {
+    	return;
+    }
+
+    var matchedRoute = matchedRoutes[0]; // currently only support 1 route
+
+    var newRoute = {
+        "origin_area": matchedRoute[0].areaCode,
+        "dest_area": matchedRoute[1].areaCode,
+        "origin_locs": [placeNameDict[matchedRoute[0].placeCode]],
+        "dest_locs": [placeNameDict[matchedRoute[1].placeCode]],
+    };
+    var routes = [newRoute];
+
+
+    // possible fb geo info hiding in dest part, need to find it first
+    // var destTextTokens = wpTokenizer.tokenize(destText);
+    // console.log("destTextTokens: " + destTextTokens);
+    // for (var i = 0; i < destTextTokens.length; i++) {
+    //     var token = destTextTokens[i];
+    //     if (token == ' - ' && i + 3 < destTextTokens.length) { // found suspicious fb geoinfo
+    //         if (nlp(destTextTokens[i + 1], fbGeoInfoLexicon).places().data().length > 0 && // next token is a place
+    //             destTextTokens[i + 2] == ', ' &&
+    //             nlp(destTextTokens[i + 3], fbGeoInfoLexicon).places().data().length > 0) {
+    //             destTextTokens = destTextTokens.slice(0, i);
+    //             break;
+    //         }
+    //     }
+    // }
+    // var dests = nlp(destTextTokens.join(' '), routePlaceLexicon).match('#MyPlace').data().map(function(matchToken) {
+    //     return matchToken.normal;
+    // });
+
+    // console.log("origins: " + origins);
+    // console.log("dests: " + dests);
+
+    // // get area string
+    // // get unique areas
+    // var originAreas = origins.slice(0),
+    //     destAreas = dests.slice(0);
+    // originAreas = removeDuplicatesBy(originAreas, function(place) {
+    //     return places[place].areaCode;
+    // });
+    // destAreas = removeDuplicatesBy(destAreas, function(place) {
+    //     return places[place].areaCode;
+    // });
+
+    // console.log("origin areas: " + originAreas);
+    // console.log("dest areas: " + destAreas);
+
+    // generate routes
+    // var routes = [];
+    // originAreas.forEach(function(originArea) {
+    //     destAreas.forEach(function(destArea) {
+    //         if (originArea != destArea) {
+    //             // get specific location strings
+    //             var originLocStrs, destLocStrs;
+    //             // get unique places
+    //             var originPlaces = origins.filter(function(place) {
+    //                 return places[place].areaCode == originArea;
+    //             });
+    //             var destPlaces = dests.filter(function(place) {
+    //                 return places[place].areaCode == destArea;
+    //             });
+    //             originPlaces = removeDuplicatesBy(originPlaces, function(place) {
+    //                 return places[place].placeCode;
+    //             });
+    //             destPlaces = removeDuplicatesBy(destPlaces, function(place) {
+    //                 return places[place].placeCode;
+    //             });
+    //             // if places fit in two lines
+    //             if (originPlaces.length <= 2 && destPlaces.length <= 2) {
+    //                 // sort by place level
+    //                 originPlaces.sort(placeCodeLvlComp);
+    //                 destPlaces.sort(placeCodeLvlComp);
+    //                 // generate user-friendly location strings
+    //                 originLocStrs = originPlaces.map(function(placeCode) {
+    //                     return placeNameDict[placeCode];
+    //                 });
+    //                 destLocStrs = destPlaces.map(function(placeCode) {
+    //                     return placeNameDict[placeCode];
+    //                 });
+    //             }
+
+    //             var newRoute = {
+    //                 "origin_area": originArea,
+    //                 "dest_area": destArea,
+    //                 "origin_locs": originLocStrs,
+    //                 "dest_locs": destLocStrs,
+    //             };
+    //             routes.push(newRoute);
+    //         }
+    //     });
+    // })
+
+    // get date and time
+    // get date
+    // var date = nlp(text).dates().data()[0].date;
+    // var dateStr;
+    // if (date.month != undefined && date.date != undefined) {
+    //     dateStr = MONTHS[date.month] + " " + date.date + " ";
+    // } else if (date.weekday != undefined) {
+    //     dateStr = DAYS_OF_WEEK[date.weekday] + " ";
+
+    // }
+    // dateStr += date.time.hour + ":" + (date.time.minute ? date.time.minute.slice(-2) : "00");
+
+    var timeStr = getTimeStr(text);
+    var dateStr = getDateStr(text, updatedTime) + " " + (timeStr ? timeStr : "");
+    console.log("date: " + dateStr);
+    var standardDate = sugar.Date.create(dateStr);
+    // var dateTokens = tokenizer.tokenize(nlp(text).match('#Date').out());
+    // console.log(dateTokens);
+    // for (var i = 0; i < dateTokens.length; i++) {
+    //     if (isDayOfWeek(dateTokens[i])) {
+    //         dateTokens.splice(i, 1);
+    //         break;
+    //     }
+    // }
+    // var date = dateTokens.join(' ');
+    // var timeMatches = text.match(new RegExp('[0-9]{1,2}(:[0-9]{2,2})? ?(am|pm)'));
+    // var standardDate;
+    // if (timeMatches.length > 0) {
+    //     standardDate = sugar.Date.create(date + " " + timeMatches[0]);
+    // }
+
+    // get price
+    var priceMatches = text.match(new RegExp('\\$[0-9]+'));
+    var price;
+    if (priceMatches) {
+        price = priceMatches[0];
+    }
+
+    var msg = {
+        "data": {
+            "post_offering": true,
+            "routes": routes,
+            "date": standardDate,
+            "price": price
+        }
+    };
+
+    return msg;
+}
+
+function removeDuplicatesBy(arr, func) {
+    var mappedArr = arr.map(func);
+    return mappedArr.filter(function(item, pos) {
+        return mappedArr.indexOf(item) == pos;
+    })
+}
+
+function placeCodeLvlComp(pc1, pc2) {
+    return placeLvlDict[pc2] - placeLvlDict[pc1];
+}
+
+// var myMatch = matchWithLexicon("waterloo -> toronto; toronto -> mississauga; stc->finch", "#place#( ?- ?| ?> ?| ?-*> ?| to )#place#", 'i', 0);
+// console.log(myMatch);
+function matchWithLexicon(src, regExpStr, options, recurCount) {
+    var tag = regExpStr.match(/#[^#]+#/);
+    tag = tag ? tag[0] : null;
+    switch (tag) {
+        case "#place#":
+            var routes = [];
+            for (var key in places) {
+                var matchResult = matchWithLexicon(src, regExpStr.replace(/#[^#]+#/, key), options, recurCount + 1);
+                if (matchResult) {
+                    matchResult.unshift(places[key]);
+                    if (recurCount == 0) {
+                        routes.push(matchResult);
+                    } else {
+                        return matchResult;
+                    }
+                }
+            }
+            if (recurCount == 0) {
+                routes.sort(function(r1, r2) {
+                    return r1[r1.length - 1] - r2[r2.length - 1];
+                }).map(function(r) {
+                    return r.splice(-1, 1);
+                });
+                return routes;
+            }
+            break;
+        default:
+            var matchResult = src.match(new RegExp(regExpStr, options));
+            return matchResult ? [matchResult.index] : null;
+    }
+    return null;
+}
+
+// console.log(getDateStr("Looking for a ride from Scarbrough/STC to Waterloo Friday afternoon."));
+function getDateStr(text, updatedTime) {
+    // exact date first
+    var dateMatch = text.match(dateRegExp);
+    if (dateMatch) {
+        var dateStr = dateMatch[0];
+        dateStr = dateStr.replace(/[^0-9](?=[0-9])/, " "); // add space between month and date
+        return dateStr;
+    }
+
+    // day of week
+    var dayOfWeekMatch = text.match(dayOfWeekRegExp);
+    if (dayOfWeekMatch) {
+        // currently only support one date
+        if (dayOfWeekMatch.length == 1) {
+            // remove extra whitespace from matching and convert to lower case
+            return myDayOfWeekDict[dayOfWeekMatch[0].replace(/ /g, "").toLowerCase()];
+        }
+        return;
+    }
+
+    // natural words like "tomorrow" ...
+    var updatedDate = new Date(updatedTime);
+    if (text.match(/(today|this afternoon|this evening|tonight)/i)) {
+        var updatedMonth = parseInt(updatedDate.getMonth()) + 1;
+        return updatedDate.getFullYear() + "-" + updatedMonth + "-" + updatedDate.getDate();
+    }
+    if (text.match(/(tomorrow)/i)) {
+        updatedDate = new Date(updatedDate.getTime() + 24 * 60 * 60 * 1000);
+        var updatedMonth = parseInt(updatedDate.getMonth()) + 1;
+        return updatedDate.getFullYear() + "-" + updatedMonth + "-" + updatedDate.getDate();
+    }
+
+    // maybe date is omitted, return "today"
+    return "today";
+}
+
+function getTimeStr(text) {
+    // hour, minute, 12 hour
+    var hm12 = text.match(/[0-9]{1,2} ?: ?[0-9]{2} ?(am|pm)/i);
+    if (hm12) {
+        return hm12[0];
+    }
+
+    // hour, 12 hour
+    var h12 = text.match(/[0-9]{1,2} ?(am|pm)/i);
+    if (h12) {
+        return h12[0];
+    }
+
+    // hour, minute, 24 hour, 1pm to 11pm
+    var hm24p = text.match(/(1[3-9]|2[0-3]) ?: ?[0-9]{2}/);
+    if (hm24p) {
+        return hm24p[0];
+    }
+
+    // look for words that describes am/pm
+    var ampm = 0; // 0 - undetermined; 1 - am; 2 - pm
+    if (text.match(/morning/i)) {
+        ampm = 1;
+    } else if (text.match(/(afternoon|night|evening)/i)) {
+        ampm = 2;
+    }
+
+    // hour, minute
+    var hm = text.match(/[0-9]{1,2} ?: ?[0-9]{2}/);
+    if (hm) {
+        switch (ampm) {
+            case 0:
+                break;
+            case 1:
+                return hm[0] + " am";
+                break;
+            case 2:
+                return hm[0] + " pm";
+                break;
+            default:
+                break
+        }
+    }
+
+    // hour
+    var hour = text.match(/[0-9]{1,2}/);
+    if (hour) {
+        switch (ampm) {
+            case 0:
+                break;
+            case 1:
+                return hour[0] + " am";
+                break;
+            case 2:
+                return hour[0] + " pm";
+                break;
+            default:
+                break
+        }
+    }
+
+}
